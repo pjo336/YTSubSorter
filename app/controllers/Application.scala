090278af782a07b40ef9acb8cdc23afe4386c098
Peter <pemjohns@gmail.com>
2014-07-23 00:26:32 -0500

    Current impl creates a tuple (ChannelName, videoId) for the last 5 videos on each channel subscribed to

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,24 +8,45 @@ import scala.collection.JavaConversions._
 object Application extends Controller {
 
   def index = Action {
+    // TODO needs heavy refactoring
     val youtubeService = new Account().getReadAndWriteAccount
     val subscriptionFinder = new SubscriptionFinder
     val subscriptionInfo = subscriptionFinder.getSubscriptionInformationTuple(youtubeService)
       .map(sub => UserSubscription(sub._1, sub._2))
-    // userSubscriptions is a list of tuples of all subscriptions as (id, title)
-    val userSubscriptions = subscriptionInfo.toList
+    // userSubsList is a list of tuples of all subscriptions as (id, title)
+    val userSubsList = subscriptionInfo.toList
 
-    val rhino = userSubscriptions(0)
-    val channelSubscribedTo = Channel.getChannelOfSubscriptionByTitle(youtubeService, rhino.title)
-    val channelDetails = channelSubscribedTo.getContentDetails
-    val channelsUploadsPlaylistId = channelDetails.getRelatedPlaylists.getUploads
-    val playlist = youtubeService.playlistItems().list("contentDetails").setPlaylistId(channelsUploadsPlaylistId).execute()
-    val firstId = playlist.getItems.toList(0).getContentDetails.getVideoId
+    var videoList:List[(String, String)] = List()
+    userSubsList.map(sub => {
+      println()
+      println("Parsing User - " + sub.title)
+      println()
+      val channelResponse = Channel.getChannelOfSubscriptionByTitle(youtubeService, sub.title)
+      val channelSubscribedTo = channelResponse.getItems.toList
+      channelSubscribedTo.map(channel => {
+        val channelDetails = channel.getContentDetails
+        val channelsUploadsPlaylistId = channelDetails.getRelatedPlaylists.getUploads
+        val playlist = youtubeService.playlistItems().list("contentDetails, " +
+          "snippet").setPlaylistId(channelsUploadsPlaylistId)
+          .execute().getItems.toList
+        playlist.map(item => {
+          val details = item.getContentDetails
+          println(sub.title + ": " + details.getVideoId + " created on: " + item.getSnippet.getPublishedAt)
+          videoList = (sub.title, details.getVideoId) :: videoList
+        })
+      })
+    })
+
+    println()
+    println("Final List: ")
+    println(videoList)
+
+
     // Etho video id
     val key = "EaYjtR105XQ"
     // original Google dev vieo id
     val originalKey = "M7lc1UVf-VE"
-    Ok(views.html.index(firstId, userSubscriptions))
+    Ok(views.html.index(key, userSubsList))
   }
 
 }
--- app/models/Channel.scala
+++ app/models/Channel.scala
@@ -6,13 +6,13 @@ import scala.collection.JavaConversions._
 trait SubscriptionChannel {
   def getChannelOfSubscriptionByTitle(ytService: YouTube, title: String) = {
     ytService.channels().list("snippet,contentDetails").setForUsername(title)
-      .execute().getItems.toList(0)
+      .execute()
   }
 
   // TODO this retrieves the authorized users channel for some reason
   def getChannelOfSubscriptionById(ytService: YouTube, id: String) = {
     ytService.channels().list("contentDetails").setId(id)
-      .execute().getItems.toList(0)
+      .execute()
   }
 }
 
